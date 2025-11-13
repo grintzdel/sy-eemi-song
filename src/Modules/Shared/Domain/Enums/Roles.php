0a290de54f41730645edb33fb7d5e971e056285d
maoudin <mathis.oudin42@gmail.com>
2025-11-13 10:45:36 +0100

    feat: add user roles

---
+++ src/Modules/Shared/Domain/Enums/Roles.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Modules\Shared\Domain\Enums;
+
+enum Roles: string
+{
+    case ROLE_ADMIN = 'ROLE_ADMIN';
+
+    case ROLE_MODERATOR = 'ROLE_MODERATOR';
+
+    case ROLE_USER = 'ROLE_USER';
+}
