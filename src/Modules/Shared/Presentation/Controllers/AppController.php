a6ffe2713d42a95c4c56ceae592df33a7227b277
maoudin <mathis.oudin42@gmail.com>
2025-11-13 10:59:14 +0100

    feat: add AppController

---
+++ src/Modules/Shared/Presentation/Controllers/AppController.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace App\Modules\Shared\Presentation\Controllers;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Messenger\Exception\ExceptionInterface;
+use Symfony\Component\Messenger\MessageBusInterface;
+use Symfony\Component\Messenger\Stamp\HandledStamp;
+
+abstract class AppController extends AbstractController {
+    private MessageBusInterface $commandBus;
+    private MessageBusInterface $queryBus;
+
+    public function __construct(MessageBusInterface $commandBus, MessageBusInterface $queryBus) {
+        $this->commandBus = $commandBus;
+        $this->queryBus = $queryBus;
+    }
+
+    /**
+     * @throws ExceptionInterface
+     */
+    public function dispatch($command): JsonResponse
+    {
+        $envelope = $this->commandBus->dispatch($command);
+        $response = $envelope->last(HandledStamp::class)->getResult();
+
+        return $this->json($response);
+    }
+
+    /**
+     * @throws ExceptionInterface
+     */
+    public function dispatchQuery($query): JsonResponse {
+        $envelope = $this->queryBus->dispatch($query);
+        $response = $envelope->last(HandledStamp::class)->getResult();
+
+        return $this->json($response);
+    }
+}
